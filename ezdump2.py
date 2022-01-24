2a46b66e498b18c8d6410178a5e8f6d052558820
vavarier <valentin.nouri20@gmail.com>
2022-01-24 16:19:46 +0100

    test

--- ezdump2.py
+++ ezdump2.py
@@ -30,6 +30,7 @@ def creat_directory(argv):
         i += 1
     print(Colors.OK + "Copying dump_tools and files in kit" + Colors.END)
     os.system("cp -r dump_tools dumpkit")
+    os.system("cp -r yay-git dumpkit")
     os.system("cp -r dumpfile dumpkit")
     print(Colors.OK + "Creating tar_file" + Colors.END)
     os.system("tar -czf dumpkit.tar dumpkit")
@@ -37,15 +38,25 @@ def creat_directory(argv):
     os.system("rm -rf dumpfile")
     print(Colors.OK + "Finish" + Colors.END)
 def unpack_dump(argv):
-    a = 0
+    if os.path.exists("dumpkit.tar"):
+        print(Colors.OK + "Untar file" + Colors.END)
+        os.system("tar -xf dumpkit.tar")
+        os.system("cp -r dumpkit/dump_tools/ezdump dumpkit/dump_tools/pkg_lists dumpkit/dump_tools/yay-git .")
+        os.system("chmod 777 ezdump")
+        os.system("./ezdump")
+        os.system("rm -rf ezdump pkg_lists")
+    else:
+        print(Colors.FAIL + "[FAIL] no dumpkit in actual folder" + Colors.END)
 
 if (len(argv) < 2):
-    a = 0
-    #print usage
+    print("type : 1 to creat kit 2 to unpack kit")
+    print("usage :\n ./ezdump2.py 'type' 'all file you whant to keep'")
+    exit(84)
+
 if (int(argv[1]) == 1):
     creat_directory(argv)
 elif (int(argv[1]) == 2):
     unpack_dump(argv)
 else:
-    a = 0
-    #print usage
+    print("type : 1 to creat kit 2 to unpack kit")
+    print("usage :\n ./ezdump2.py 'type' 'all file you whant to keep'")
